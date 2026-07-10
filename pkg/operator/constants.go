2cc2b13865405921c17356fa9b9e8335c189b12e
jeancdev <jcode2006@gmail.com>
2026-07-09 20:34:18 -0500

    feat(constants): add Prebuild labels and annotations for improved resource management

--- pkg/operator/constants.go
+++ pkg/operator/constants.go
@@ -6,10 +6,17 @@ const (
 	Kind       = "Workspace"
 	Plural     = "workspaces"
 
+	PrebuildKind  = "Prebuild"
+	PrebuildPlural = "prebuilds"
+
 	LabelWorkspaceID = "nimbuscore.io/workspace-id"
 	LabelUserID      = "nimbuscore.io/user-id"
 	LabelTeamID      = "nimbuscore.io/team-id"
+	LabelPrebuildID  = "nimbuscore.io/prebuild-id"
 
-	AnnotationRepoURL = "nimbuscore.io/repo-url"
-	AnnotationBranch  = "nimbuscore.io/branch"
+	AnnotationRepoURL        = "nimbuscore.io/repo-url"
+	AnnotationBranch         = "nimbuscore.io/branch"
+	AnnotationPorts          = "nimbuscore.io/ports"
+	AnnotationIdleTimeout    = "nimbuscore.io/idle-timeout"
+	AnnotationPrebuildOutput = "nimbuscore.io/prebuild-output"
 )
